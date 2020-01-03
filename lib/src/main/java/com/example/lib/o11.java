e276a906e4faec9ca52573caf53b8e9dea446b6b
ffrank2589 <ffrank2589@yahoo.com.tw>
2020-01-03 14:57:41 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/o11.java
@@ -0,0 +1,10 @@
+package com.example.lib;
+
+import java.util.Scanner;
+
+public class o11 {
+    public static void main(String[] args) {
+        num prizeNumber = new num("7_8.txt");
+        prizeNumber.Judge();
+    }
+}
